7096f42fa13d4579e82a1a2cc37ae1ed1659c956
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-03-17 11:33:04 +0900

    Revert "Update test refs"
    This reverts commit 6da3fb2f70f53b3c7ab9d54671e8f9fee2813fab.

--- crates/swc_ecma_minifier/tests/pass-1/1/output.js
+++ crates/swc_ecma_minifier/tests/pass-1/1/output.js
@@ -11,4 +11,7 @@ foo();
 condString.toUpperCase, foo();
 condString.toUpperCase, foo(), foo();
 condString.toUpperCase, foo(), 0;
+condString.toUpperCase, foo();
+foo();
+condString.toUpperCase, foo();
 condString.toUpperCase;
