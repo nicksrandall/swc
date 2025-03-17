a0737a861c962a5daf5a77ba934dc96bb08564bc
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-03-17 12:06:55 +0900

    Update test refs

--- crates/swc_ecma_minifier/tests/pass-1/1/output.js
+++ crates/swc_ecma_minifier/tests/pass-1/1/output.js
@@ -14,4 +14,6 @@ condString.toUpperCase, foo(), 0;
 condString.toUpperCase, foo();
 foo();
 condString.toUpperCase, foo();
+condString.toUpperCase, foo(), foo();
+condString.toUpperCase, foo(), 0;
 condString.toUpperCase;
